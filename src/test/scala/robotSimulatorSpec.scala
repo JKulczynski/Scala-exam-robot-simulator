7d1e9795a7c171dde0912bd81afafc03a6500030
JKulczynski <j.kulczynski@adform.com>
2022-04-01 14:18:13 +0200

    robotSimulatorSpec

---
+++ src/test/scala/robotSimulatorSpec.scala
@@ -0,0 +1,8 @@
+import org.scalatest.flatspec.AnyFlatSpec
+
+case class robotSimulatorSpec extends AnyFlatSpec{
+
+  behavior of "robotSimulator"
+
+  it should "have three possible movements: turn right, turn left and advance"
+}
